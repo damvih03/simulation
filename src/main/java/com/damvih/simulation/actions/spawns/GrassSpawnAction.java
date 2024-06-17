d768580a26c0726aa212723260621513d8425bc5
damvih <damvih03.official@gmail.com>
2024-06-17 14:26:52 +1000

    Remove unnecessary import

--- src/main/java/com/damvih/simulation/actions/spawns/GrassSpawnAction.java
+++ src/main/java/com/damvih/simulation/actions/spawns/GrassSpawnAction.java
@@ -1,6 +1,5 @@
 package com.damvih.simulation.actions.spawns;
 
-import com.damvih.simulation.Coordinates;
 import com.damvih.simulation.world_map.WorldMap;
 import com.damvih.simulation.entities.Entity;
 import com.damvih.simulation.entities.Grass;
